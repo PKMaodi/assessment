9d16bb59d19ae06ed33d87b34d282993402e63a5
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-03 13:53:12 +0200

    Added the provided compulsory code.

---
+++ src/main/java/com/enviro/assessment/grad001/paulmaodi/assessment/ImageController.java
@@ -0,0 +1,18 @@
+package com.enviro.assessment.grad001.paulmaodi.assessment;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/image")
+public class ImageController {
+    
+    @GetMapping(value = "/{name}/{surname}/{\\w\\. \\w}")
+    public FileSystemResource gethttpAddedImageLink(@PathVariable String name, @PathVariable String surname){
+        //return the result
+
+    }
+}
