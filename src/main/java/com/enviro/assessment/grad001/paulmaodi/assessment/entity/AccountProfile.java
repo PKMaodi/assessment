4f8b04bcbe43caed30f63271ae6783f6402a2f5e
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-04 05:58:57 +0200

    AccountProfile class created.

--- src/main/java/com/enviro/assessment/grad001/paulmaodi/assessment/entity/AccountProfile.java
+++ src/main/java/com/enviro/assessment/grad001/paulmaodi/assessment/entity/AccountProfile.java
@@ -1,9 +1,49 @@
 package com.enviro.assessment.grad001.paulmaodi.assessment.entity;
 
-import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class AccountProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
     
+    private String surname;
+      
+    String httpImageLink;
+
+    public String getHttpImageLink() {
+        return httpImageLink;
+    }
+    public void setHttpImageLink(String httpImageLink) {
+        this.httpImageLink = httpImageLink;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    
+
 }
