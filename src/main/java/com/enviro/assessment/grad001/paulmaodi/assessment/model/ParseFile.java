9f29ac9649be5909485834efd404e9651c5768b7
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-04 06:32:44 +0200

    H2 implementation begun.

--- src/main/java/com/enviro/assessment/grad001/paulmaodi/assessment/model/ParseFile.java
+++ src/main/java/com/enviro/assessment/grad001/paulmaodi/assessment/model/ParseFile.java
@@ -3,31 +3,48 @@ package com.enviro.assessment.grad001.paulmaodi.assessment.model;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.enviro.assessment.grad001.paulmaodi.assessment.entity.AccountProfile;
 
 public class ParseFile implements FileParser{
     //The file extension type to be read from the csv file
     private String fileExtension;
 
     @Override
-    public void parseCSV(File csvFile) {
+    public void parseCSV(File csvFile) throws IOException{
         //Read the CSV records
         FileReader flReader;
         BufferedReader buReader;
         String lineString;
         String[] records;
-        try{
-            flReader = new FileReader(csvFile);
-            buReader = new BufferedReader(flReader);
-            lineString = buReader.readLine();
-            while(lineString != null){
-                records = lineString.split(",");
-                
-            }
+
+        List<AccountProfile> profiles = new ArrayList<>();
+        flReader = new FileReader(csvFile);
+        buReader = new BufferedReader(flReader);
+        lineString = buReader.readLine();
+
+        while(lineString != null){
+            records = lineString.split(",");
+            
+            //Creating an Account Profile
+            AccountProfile accountProfile = new AccountProfile();
+            accountProfile.setName(records[0]);
+            accountProfile.setSurname(records[1]);
+            fileExtension = records[2];
+            accountProfile.setHttpImageLink(records[3]);
+            
+            //Save each profile
+            profiles.add(accountProfile);
+
         }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
+
+        //Persist the profiles to the database
+        
+        
         
         
         
